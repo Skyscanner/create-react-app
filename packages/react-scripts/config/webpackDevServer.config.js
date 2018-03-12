e5fbc77443e3a61c4f287f96616e6a8aab5d7713
David Braden <david.braden@skyscanner.net>
2018-03-12 14:40:16 +0000

    Allow CORS requests to dev-server

--- packages/react-scripts/config/webpackDevServer.config.js
+++ packages/react-scripts/config/webpackDevServer.config.js
@@ -85,6 +85,12 @@ module.exports = function(proxy, allowedHost) {
     },
     // Enable HTTPS if the HTTPS environment variable is set to 'true'
     https: protocol === 'https',
+    // Allow CORS requests
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, PATCH, OPTIONS',
+      'Access-Control-Allow-Headers': 'X-Requested-With, content-type, Authorization'
+    },
     host: host,
     overlay: false,
     historyApiFallback: {
