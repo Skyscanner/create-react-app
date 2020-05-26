eb4c663e15918d7ceff7c6ef26c67d532ef104ff
artemtsvetkov <artem.tsvetkov@skyscanner.net>
2020-05-26 18:16:14 +0200

    CORS

--- packages/react-scripts/config/webpack.config.js
+++ packages/react-scripts/config/webpack.config.js
@@ -192,7 +192,7 @@ module.exports = function(webpackEnv) {
       // changing JS code would still trigger a refresh.
     ].filter(Boolean),
     output: {
-      crossOriginLoading: 'anonymous',
+      crossOriginLoading,
       // The build folder.
       path: isEnvProduction ? paths.appBuild : undefined,
       // Add /* filename */ comments to generated require()s in the output.
